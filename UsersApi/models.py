07403a3540fa080b7e63d0260d63a3a3d2787493
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2022-03-11 12:19:13 +0530

    Added MyUser Model and created get/post/delete api

--- UsersApi/models.py
+++ UsersApi/models.py
@@ -1,11 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class MyUserManager(BaseUserManager):
+	def create_user(self, phone, email, first_name, last_name, password=None):
+		
+		if not phone:
+			raise ValueError("Phone Number is required")
+		if not email:
+			raise ValueError("Email is required")
+		if not first_name:
+			raise ValueError("First name is required")
+		if not last_name:
+			raise ValueError("Last name is required")
 
-class User(models.Model):
-	# add more relevant fields
-	name = models.CharField(max_length=50)
+		user = self.model(
+			email = self.normalize_email(email),
+			first_name = first_name,
+			last_name = last_name,
+			phone = phone
+		)
 
-	# change as per use
+		user.set_password(password)
+		user.save(using = self.db)
+
+		return user
+
+	def create_superuser(self, email, phone, first_name, last_name, password=None):
+		user = self.create_user(
+			email = self.normalize_email(email),
+			first_name = first_name,
+			last_name = last_name,
+			password = password,
+			phone = phone
+		)
+
+		user.is_admin = True
+		user.is_superuser = True
+		user.is_staff = True
+		user.save(using = self.db)
+		return user
+
+
+class MyUser(AbstractBaseUser):
+
+	USER_CATEGORY = (
+		('Customer', 'Customer'),
+		('Staff', 'Staff'),
+		('Delivery', 'Delivery')
+	)
+
+	email = models.EmailField(verbose_name="Email Address", max_length=200, unique=True)
+	first_name = models.CharField(verbose_name="First Name", max_length=200, null=True, blank=True)
+	middle_name = models.CharField(verbose_name="Middle Name", max_length=200, null=True, blank=True)
+	last_name = models.CharField(verbose_name="Last Name", max_length=200, null=True, blank=True)
+	phone = models.BigIntegerField(verbose_name="Contact Number", null=True, blank=True, unique=True)
+	address = models.TextField(null=True, blank=True)
+	age = models.IntegerField(verbose_name="Age", null=True, blank=True)
+	gender = models.CharField(verbose_name="Gender", max_length=200, null=True, blank=True)
+	user_category = models.CharField(verbose_name="User Category", choices=USER_CATEGORY, default='Customer', max_length=200)
+	current_location = models.DecimalField(verbose_name="Current Location",max_digits=22,
+    decimal_places=16, null=True, blank=True)
+	last_updated_location = models.DecimalField(verbose_name="Last Updated Location",max_digits=22,
+    decimal_places=16, null=True, blank=True)
+
+	is_admin = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+	is_superuser = models.BooleanField(default=False)
+
+	USERNAME_FIELD = "email"
+
+	REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'phone']
+
+	objects = MyUserManager()
+
 	def __str__(self):
-		return self.name
+		return self.email
+
+	def has_perm(self, perm, obj=None):
+		return True
+	
+	def has_module_perms(self, app_label):
+		return True
--- UsersApi/views.py
+++ UsersApi/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer
-
-from .models import User
+from .serializers import MyUserSerializer
+from rest_framework.views import APIView
+from .models import MyUser
+from rest_framework.pagination import PageNumberPagination
 
 # This is a sample working of API
 # create like this for other APIs
@@ -13,3 +14,50 @@ from .models import User
 @api_view(['GET'])
 def testApi(request):
 	return Response("Api Working!!")
+
+@api_view(['GET'])
+def getUsers(request):
+	paginator = PageNumberPagination()
+	if 'page_size' in request.GET:
+		paginator.page_size = int(request.GET['page_size'])
+	else:
+		paginator.page_size = 10
+	users = MyUser.objects.all()
+	result_page = paginator.paginate_queryset(users, request)
+	serializer = MyUserSerializer(result_page, many=True)
+	return paginator.get_paginated_response(serializer.data)
+
+
+@api_view(['POST'])
+def createUser(request):
+	serializer = MyUserSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+	else:
+		return Response({'message':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+	
+	return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteUser(request, pk):
+	try:
+		user = MyUser.objects.get(id=pk)
+	except Exception as e:
+		return Response({'message':str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+	user.delete()
+
+	return Response({'message':'User data deleted'})
+
+
+@api_view(['GET'])
+def getSpecificUser(request, pk):
+	try:
+		user = MyUser.objects.get(id=pk)
+	except Exception as e:
+		return Response({'message':str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+	serializer = MyUserSerializer(user)
+
+	return Response(serializer.data)
