d3344fce20117d59bc0614f3952f4825d5af50b6
Snehal Swadhin <snehalswadhin@gmail.com>
2022-03-28 03:09:38 +0530

    Changed urls.py

--- UsersApi/urls.py
+++ UsersApi/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
 	path('logout/', views.BlacklistRefreshView.as_view(), name="logout"),
 	path('register/', views.register, name="register"),
 	path('add/<str:pk>/', views.addEmployee, name="addEmployees"),
-	path('get_closest_delivery_partner', views.closestDeliveryPartner, name='get_closest_delivery_partner'),
-	path('usershistory', views.usershistory, name="usersHistory"),
+	path('delivery_partner/', views.closestDeliveryPartner, name='get_closest_delivery_partner'),
+	path('usershistory/', views.usershistory, name="usersHistory"),
 ]
