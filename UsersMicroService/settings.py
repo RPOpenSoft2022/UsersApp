6303dc725b560f493616f7b3ca6c64c57d400e80
shobhi10058 <rkgupta462gkp@gmail.com>
2022-03-28 16:25:09 +0530

    refresh life to 2hrs

--- UsersMicroService/settings.py
+++ UsersMicroService/settings.py
@@ -85,7 +85,7 @@ AUTH_USER_MODEL = 'UsersApi.User'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=120),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
